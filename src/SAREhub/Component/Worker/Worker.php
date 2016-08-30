2ea6982b46b54499a22bf68e340af9761077b6b5
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-08-30 14:48:21 +0200

    changed Worker class to interface

--- src/SAREhub/Component/Worker/Worker.php
+++ src/SAREhub/Component/Worker/Worker.php
@@ -5,30 +5,21 @@ namespace SAREhub\Component\Worker;
 /**
  * Represents Worker instance.
  */
-abstract class Worker {
-	
-	/** @var WorkerInfo */
-	protected $info;
-	
-	public function __construct(WorkerInfo $info) {
-		$this->info = $info;
-	}
+interface Worker {
 	
 	/**
-	 * Implements worker work logic code.
+	 * Executed once on worker start.
 	 */
-	public abstract function work();
+	public function onStart();
+	
+	/**
+	 * Executed on every tick, must implements worker logic code.
+	 */
+	public function onTick();
 	
 	/**
 	 * Executed when worker was stopped.
 	 * @param array $parameters Optional array with parameters.
 	 */
-	public abstract function onStop(array $parameters);
-	
-	/**
-	 * @return WorkerInfo
-	 */
-	public function getInfo() {
-		return $this->info;
-	}
+	public function onStop(array $parameters);
 }
