ca4b1fdcf9d353e2308bdb756a3406de049b3b6c
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-08-30 15:11:35 +0200

    added WorkerRunner class

---
+++ src/SAREhub/Component/Worker/WorkerRunner.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SAREhub\Component\Worker;
+
+use SAREhub\Component\Worker\Command\StandardWorkerCommands;
+use SAREhub\Component\Worker\Command\WorkerCommand;
+use SAREhub\Component\Worker\Command\WorkerCommandInput;
+
+class WorkerRunner {
+	
+	/** @var Worker */
+	protected $worker;
+	
+	/** @var WorkerCommandInput */
+	protected $commandInput;
+	
+	/** @var bool */
+	protected $running = false;
+	
+	public function __construct(Worker $worker, WorkerCommandInput $commandInput) {
+		$this->worker = $worker;
+		$this->commandInput = $commandInput;
+	}
+	
+	public function start() {
+		if (!$this->isRunning()) {
+			$this->worker->onStart();
+			$this->running = true;
+		}
+	}
+	
+	public function tick() {
+		if ($this->isRunning()) {
+			if ($command = $this->commandInput->getNextCommand()) {
+				$this->processCommand($command);
+				if (!$this->isRunning()) {
+					return;
+				}
+			}
+			
+			$this->worker->onTick();
+		}
+	}
+	
+	protected function processCommand(WorkerCommand $command) {
+		switch ($command->getName()) {
+			case StandardWorkerCommands::STOP_COMMAND_NAME:
+				$this->stop($command->getParameters());
+				break;
+		}
+	}
+	
+	public function stop(array $parameters) {
+		$this->worker->onStop($parameters);
+		$this->running = false;
+	}
+	
+	public function isRunning() {
+		return $this->running;
+	}
+}
---
+++ tests/unit/SAREhub/Component/Worker/WorkerRunnerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SAREhub\Component\Worker;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
+use SAREhub\Component\Worker\Command\StandardWorkerCommands;
+use SAREhub\Component\Worker\Command\WorkerCommand;
+use SAREhub\Component\Worker\Command\WorkerCommandInput;
+
+class WorkerRunnerTest extends TestCase {
+	
+	/** @var PHPUnit_Framework_MockObject_MockObject */
+	private $workerMock;
+	
+	/** @var PHPUnit_Framework_MockObject_MockObject */
+	private $commandInputMock;
+	
+	/** @var WorkerRunner */
+	private $workerRunner;
+	
+	protected function setUp() {
+		$this->workerMock = $this->getMockBuilder(Worker::class)->getMock();
+		$this->commandInputMock = $this->getMockBuilder(WorkerCommandInput::class)->getMock();
+		$this->workerRunner = new WorkerRunner($this->workerMock, $this->commandInputMock);
+	}
+	
+	public function testStart() {
+		$this->workerMock->expects($this->once())->method('onStart');
+		$this->workerRunner->start();
+		$this->workerRunner->start();
+		$this->assertTrue($this->workerRunner->isRunning());
+	}
+	
+	public function testTick() {
+		$this->workerMock->expects($this->once())->method('onTick');
+		$this->workerRunner->start();
+		$this->commandInputMock->expects($this->once())->method('getNextCommand')->willReturn(null);
+		$this->workerRunner->tick();
+	}
+	
+	public function testStop() {
+		$this->workerMock->expects($this->once())->method('onStop');
+		$this->workerRunner->start();
+		$this->workerRunner->stop([]);
+		$this->assertFalse($this->workerRunner->isRunning());
+	}
+	
+	public function testProcessStopCommand() {
+		$this->workerMock->expects($this->once())->method('onStop');
+		$stopWorkerCommand = $this->getMockBuilder(WorkerCommand::class)->disableOriginalConstructor()->getMock();
+		$stopWorkerCommand->method('getName')->willReturn(StandardWorkerCommands::STOP_COMMAND_NAME);
+		$stopWorkerCommand->method('getParameters')->willReturn([]);
+		$this->commandInputMock->expects($this->once())->method('getNextCommand')->willReturn($stopWorkerCommand);
+		
+		$this->workerRunner->start();
+		$this->workerRunner->tick();
+		$this->assertFalse($this->workerRunner->isRunning());
+	}
+	
+}
