5a0e7d4638d9b16962e1de46db7d0e2bcc3d870a
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-09-05 10:36:45 +0200

    refactored to fluent interface

--- src/SAREhub/Component/Worker/WorkerInfo.php
+++ src/SAREhub/Component/Worker/WorkerInfo.php
@@ -10,6 +10,35 @@ class WorkerInfo {
 	public $uuid;
 	public $startTime;
 	
+	/**
+	 * @return WorkerInfo
+	 */
+	public static function newInfo() {
+		$info = new self();
+		return $info;
+	}
+	
+	/**
+	 * @param string $uuid
+	 * @return $this
+	 */
+	public function uuid($uuid) {
+		$this->uuid = $uuid;
+		return $this;
+	}
+	
+	/**
+	 * @param string $time
+	 * @return $this
+	 */
+	public function startTime($time) {
+		$this->startTime = $time;
+		return $this;
+	}
+	
+	/**
+	 * @return array
+	 */
 	public function toArray() {
 		return [
 		  'uuid' => $this->uuid,
