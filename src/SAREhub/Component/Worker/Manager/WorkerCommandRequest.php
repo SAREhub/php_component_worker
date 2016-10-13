9befef04017083b4cbbefcc8a5b97107655df2f5
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-10-13 14:09:01 +0200

    added WorkerCommandRequest

---
+++ src/SAREhub/Component/Worker/Manager/WorkerCommandRequest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace SAREhub\Component\Worker\Manager;
+
+
+use SAREhub\Component\Worker\Command\Command;
+
+class WorkerCommandRequest {
+	
+	const DEFAULT_REPLY_TIMEOUT = 30;
+	
+	/**
+	 * @var string
+	 */
+	private $workerId;
+	
+	/**
+	 * @var Command
+	 */
+	private $command;
+	
+	/**
+	 * @var int
+	 */
+	private $replyTimeout = self::DEFAULT_REPLY_TIMEOUT;
+	
+	/**
+	 * @var int
+	 */
+	private $sentTime = 0;
+	
+	/**
+	 * @var callable
+	 */
+	private $replyCallback;
+	
+	
+	protected function __construct() {
+		
+	}
+	
+	public static function newInstance() {
+		return new self();
+	}
+	
+	public function withWorkerId($id) {
+		$this->workerId = $id;
+		return $this;
+	}
+	
+	public function withCommand(Command $command) {
+		$this->command = $command;
+		return $this;
+	}
+	
+	public function withReplyTimeout($timeout) {
+		$this->replyTimeout = $timeout;
+		return $this;
+	}
+	
+	public function withReplyCallback(callable $callback) {
+		$this->replyCallback = $callback;
+		return $this;
+	}
+	
+	public function markAsSent($now) {
+		$this->sentTime = $now;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function isSent() {
+		return $this->getSentTime() > 0;
+	}
+	
+	/**
+	 * @param $now
+	 * @return bool
+	 */
+	public function isReplyTimeout($now) {
+		return $this->isSent() && $now >= ($this->getSentTime() + $this->getReplyTimeout());
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getWorkerId() {
+		return $this->workerId;
+	}
+	
+	/**
+	 * @return Command
+	 */
+	public function getCommand() {
+		return $this->command;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getSentTime() {
+		return $this->sentTime;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getReplyTimeout() {
+		return $this->replyTimeout;
+	}
+	
+	/**
+	 * @return callable
+	 */
+	public function getReplyCallback() {
+		return $this->replyCallback;
+	}
+	
+}
