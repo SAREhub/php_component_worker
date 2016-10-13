f17372239de15fe1ad449f6a063717f166aee98c
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-10-13 09:27:40 +0200

    fix tests

--- tests/unit/SAREhub/Component/Worker/Command/BasicCommandTest.php
+++ tests/unit/SAREhub/Component/Worker/Command/BasicCommandTest.php
@@ -7,22 +7,24 @@ use SAREhub\Component\Worker\Command\BasicCommand;
 class BasicCommandTest extends TestCase {
 	
 	public function testCreateWithoutParameters() {
-		$command = new BasicCommand('name');
+		$command = new BasicCommand('1', 'name');
+		$this->assertEquals('1', $command->getCorrelationId());
 		$this->assertEquals('name', $command->getName());
 		$this->assertEquals([], $command->getParameters());
 	}
 	
 	public function testCreateWithParameters() {
 		$parameters = ['param1' => 1];
-		$command = new BasicCommand('name', $parameters);
+		$command = new BasicCommand('1', 'name', $parameters);
 		$this->assertEquals('name', $command->getName());
 		$this->assertEquals($parameters, $command->getParameters());
 	}
 	
 	public function testToString() {
-		$command = new BasicCommand('name1', ['param1' => 1]);
+		$command = new BasicCommand('1', 'name1', ['param1' => 1]);
 		
 		$expectedJson = json_encode([
+		  'correlation_id' => '1',
 		  'name' => 'name1',
 		  'parameters' => [
 			'param1' => 1
