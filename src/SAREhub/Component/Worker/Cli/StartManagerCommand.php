5c352336a235edbe59153f10c46fbd831f3e60fe
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-10-21 14:58:42 +0200

    fixes

--- src/SAREhub/Component/Worker/Cli/StartManagerCommand.php
+++ src/SAREhub/Component/Worker/Cli/StartManagerCommand.php
@@ -2,9 +2,8 @@
 
 namespace SAREhub\Component\Worker\Cli;
 
-use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class StartManagerCommand extends CliCommand {
@@ -35,13 +34,11 @@ class StartManagerCommand extends CliCommand {
 		$this->setName('start-manager')
 		  ->setDescription('Starts selected worker manager')
 		  ->setHelp("Starts worker manager with config from file. Check example config: cli/workerManagerConfigExample.php ")
-		  ->setDefinition(new InputDefinition([
-		    new InputOption('manager', 'm', InputOption::VALUE_REQUIRED, 'manager id')
-		  ]));
+		  ->addArgument('manager', InputArgument::REQUIRED, 'manager id to start');
 	}
 	
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$managerId = $input->getOption('manager');
+		$managerId = $input->getArgument('manager');
 		$configPath = $this->getConfigPath($managerId);
 		if ($this->isConfigExists($configPath)) {
 			$this->getLogger()->info('starting manager with config ', ['config' => $configPath]);
@@ -49,12 +46,15 @@ class StartManagerCommand extends CliCommand {
 			
 			$unitName = $this->getManagerUnitInstanceName($managerId);
 			$this->getLogger()->info('manager unit instance name: ', ['unit' => $unitName]);
-			$output->writeln('manager instance unit name: '.$unitName);
+			$output->write('manager instance unit name: '.$unitName.' ');
 			
 			$return = $this->systemdHelper->start($unitName);
-			$this->getLogger()->info('systemd start output: '.$return);
-			$output->writeln('systemd start output: '.$return);
-			
+			if (!empty($return)) {
+				$this->getLogger()->info('systemd start output: '.$return);
+				$output->writeln('systemd start output: '.$return);
+			} else {
+				$output->writeln('started');
+			}
 		} else {
 			$output->writeln("config file isn't exists");
 			$this->getLogger()->warning("config file isn't exists", ['config' => $configPath]);
